81db2b1a9097a93749ee3dee67488fb46f16ae69
bauersebastian <sebastian@pisada.de>
2020-11-28 16:39:19 +0100

    fix favicon admin error

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView, TemplateView
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
@@ -26,6 +26,10 @@ urlpatterns = [
     path("users/", include("termplanner.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    path(
+        "favicon.ico/",
+        RedirectView.as_view(url=f"{settings.STATIC_URL}images/favicons/favicon.ico"),
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
