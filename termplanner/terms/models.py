0ce564b12882f2d374e7bdc44e4a937f826caa17
bauersebastian <sebastian@pisada.de>
2020-12-08 22:50:15 +0100

    fix for semestermodule object title

--- termplanner/terms/models.py
+++ termplanner/terms/models.py
@@ -68,7 +68,7 @@ class SemesterModule(TimeStampedModel):
     )
 
     def __str__(self):
-        return self.module.title
+        return self.id
 
     def get_absolute_url(self):
         """Return absolute url to the semestermodule detail page."""
