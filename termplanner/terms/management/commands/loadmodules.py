9827ce66bacb84e0060c6f5efbdd24b977f2bd3c
bauersebastian <sebastian@pisada.de>
2020-12-08 22:42:07 +0100

    migration script for loading modules

--- termplanner/terms/management/commands/loadmodules.py
+++ termplanner/terms/management/commands/loadmodules.py
@@ -2,7 +2,6 @@ import pandas as pd
 from django.core.management.base import BaseCommand
 
 from termplanner.terms.models import Module
-from termplanner.users.models import User
 
 
 class Command(BaseCommand):
@@ -17,22 +16,11 @@ class Command(BaseCommand):
         df = pd.read_csv(path, sep=";")
 
         for index, row in df.iterrows():
-            # clean dirty data
-            # try:
-            #    value = int(row["capacity"])
-            # except ValueError:
-            #    row["capacity"] = 0
-
-            # clean dirty data in fee
-            # if row["fee"] != True or row["fee"] != False or row["fee"] != None:
-            #    row["fee"] = None
 
             Module.objects.update_or_create(
-                # currently no ids for updates in file available
-                # id=row["@id"],
-                # osm_type=row["@type"],
+                # match by short_title of module
+                short_title=row["short_title"],
                 defaults=dict(
-                    created_by=User.objects.get(pk=1),
                     title=row["title"],
                     short_title=row["short_title"],
                     description=row["description"],
@@ -45,3 +33,4 @@ class Command(BaseCommand):
                     term=row["term"],
                 ),
             )
+            self.stdout.write("Saved %s" % row["title"])
